7bd1fe8af171ff4b3c5a1a394ef80db28b218579
Aditya Sharma <adityasharma2005123@gmail.com>
2025-07-15 11:09:43 +0530

    Removed the theme logic to adapt to users' system, forced dark theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning className={`dark ${inter.variable}`}>
       <body className={inter.className}>
-        <ThemeProvider attribute="class" defaultTheme="dark" forcedTheme="dark" enableSystem disableTransitionOnChange>
+        <ThemeProvider attribute="class" defaultTheme="dark" forcedTheme="dark" disableTransitionOnChange>
           {children}
         </ThemeProvider>
       </body>
